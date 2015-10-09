f3e29348a43370b01c9408e4ced082696c38e80f
Victoria Brown <victoriabrown0710@gmail.com>
2015-10-09 10:16:32 -0500

    Initial Commit

---
+++ MaximumFinder.java
@@ -0,0 +1,44 @@
+// MaximumFinder.java 10/07/15
+// Victoria Brown
+// Programmer-declared method maximum with 3 double parameters.
+import java.util.Scanner;
+
+public class MaximumFinder {
+	// obtain three floating-point values and locate the max value
+	public static void main(String[] args) {
+		
+		// create Scanner for input from command window
+		Scanner input = new Scanner(System.in);
+		
+		// prompt for and input 3 floating-point values
+		System.out.print("Enter 3 floating-point values separated by spaces: ");
+		double number1 = input.nextDouble();   // read first double
+		double number2 = input.nextDouble();   // read second double
+		double number3 = input.nextDouble();   // read third double
+		
+		// determine the maximum value
+		double result = maximum(number1, number2, number3);
+		
+		// display the maximum value
+		System.out.println("Maximum is: " + result);
+		
+	}
+	
+	// returns the maximum of its 3 double parameters
+	public static double maximum(double x, double y, double z) {
+		
+		double maximumValue = x;   // assume x is the largest to start
+		
+		// determine whether y is greater than maximumValue
+		if ( y > maximumValue) {
+			maximumValue = y; 
+		}
+		
+		// determine whether z is greater than maximumValue
+		if ( z > maximumValue) {
+			maximumValue = z; 
+		}
+		
+		return maximumValue;
+	}
+}
